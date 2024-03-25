1538517de6e3f016037689cc246cce22fe2c1568
Haris <HarisSaeed583@gmail.com>
2024-03-25 15:52:44 +0500

    fixed

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({
     path: "./data/config.env"
 })
 
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({ origin: '*' }));
